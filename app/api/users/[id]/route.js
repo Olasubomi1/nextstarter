7a86206fab2749886612c555ab91eeba169d3a9a
Olasubomi <shubbynlaa@gmail.com>
2023-09-22 15:52:05 +0100

    Created an endpoint for getting logged in user feeds.

---
+++ app/api/users/[id]/route.js
@@ -0,0 +1,16 @@
+const { connectToDB } = require("@utils/database");
+import Prompt from "@models/PromptSchema";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({
+      creator: params.id,
+    }).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+};
